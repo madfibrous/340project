13bce9712610abe20e1de1bf87922507700e9785
madfibrous <jaskrol@gmail.com>
2021-03-15 18:18:07 -0700

    Can add bike now

--- database.js
+++ database.js
@@ -96,8 +96,8 @@ const searchCustomerByPhone =
   Customers.phone = ?`;
 const insertBike = 
   `INSERT INTO Bicycles
-  (make, model, size, color, type, price, qty)
-  VALUES (?, ?, ?, ?, ?,?, ?)`;
+  (make, model, size, color, type)
+  VALUES (?, ?, ?, ?, ?)`;
 const insertService =
   `INSERT INTO Services (name, expected_turnaround, price)
   VALUES (?, ?, ?)`;
@@ -446,9 +446,8 @@ app.post('/admin',function(req,res){
         //sql
     }
     if (req.body['addBike']){
-        var sql = "INSERT INTO Bicycles (make, model, size, color, type) VALUES (?,?,?,?,?,?,?)";
         var inserts = [req.body.make, req.body.model, req.body.size, req.body.color, req.body.type];
-        sql = mysql.pool.query(sql, inserts,function(error, results,fields){
+        mysql.pool.query(insertBike, inserts,function(error, results,fields){
             if(error){
                 console.log(JSON.stringify(error));
                 res.write(JSON.stringify(error));
